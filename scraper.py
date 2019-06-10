83d917ce7d17bc8cb17612c026ed01b4495e8067
Tristan Delforge <tristandelforge1@gmail.com>
2019-06-09 21:56:49 -0500

    minor progress

--- scraper.py
+++ scraper.py
@@ -1,7 +1,27 @@
 import requests
+import random
 
-cc_path = r'C:\Users\trist\OneDrive\Documents\cc_data'
-spammer_path = 'r'
+
+
+cc_path = r'C:\Users\trist\Documents\cc_project'
+spammer_path = r'C:\Users\trist\Documents\cc_project\spammer_list'
+index = 'CC-MAIN-2019-22'
+cc_search_url = "http://index.commoncrawl.org/{0}?url={1}&matchType=domain&output=json"
+index_url = ' https://commoncrawl.s3.amazonaws.com/{0}'
+
 
 class Crawler():
-    pass
+
+    def load_labeled_websites(self, max_num = 100000, balanced = False):
+        with open('{0}/{1}'.format(cc_path, '{0}-index-locs/cc-index.paths'.format(index))) as f:
+            data = f.readlines()
+            data = [i.strip() for i in data if '.gz' in i]
+            print(data)
+
+    def get_scammer_domains(self):
+        pass
+
+
+if __name__ == '__main__':
+    c = Crawler()
+    c.load_labeled_websites()
